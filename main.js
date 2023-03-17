5ac3c01bcb85ad8aff4f8defe915580188b6c5f5
marru1423 <claritasentoy23@gmail.com>
2023-03-17 16:13:39 +0800

    hello world

--- main.js
+++ main.js
@@ -1,17 +1,17 @@
-// NAVBAR OPEN
 function toggleNav(elementID) {
   const openNav = document.getElementById(elementID)
 
   openNav.dataset.shown = openNav.dataset.shown === "false"
       ? showElement(openNav) // will return "true"
       : hideElement(openNav) // will return "false"
-}
 
+}
 function showElement(element) {
   element.classList.add('block')
   element.classList.remove('hidden')
   return "true"
 }
+
 function hideElement(element) {
   element.classList.add('hidden')
   element.classList.remove('block')
